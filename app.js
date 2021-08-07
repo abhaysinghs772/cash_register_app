af3d6e9d60b0fc07bc8535f179f224677974743b
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-08-07 23:53:06 +0530

    feat- show feature is added

--- app.js
+++ app.js
@@ -2,11 +2,32 @@
 
 // currencies
 const notes = [2000, 500, 100, 20, 10, 5, 1]
-const bill_Amount= document.querySelector('billAmount');
-const nextBtn = document.querySelector('next');
+const billAmount= document.querySelector('.billAmount');
+const nextBtn = document.querySelector('.next');
+const cashGiven = document.querySelector('.cashAmount');
+const checkBtn =document.querySelector('.check');
+const cashContainer = document.querySelector('.cashContainer')
+const returnCashContainer = document.querySelector('.returnCashContainer');
+let billAmountVal;
+let cashGivenVal;
+warningMessage = document.querySelector('.warningMessage');
 
-// console.log(bill_Amount);
-console.log(nextBtn);
-nextBtn.addEventListener('click', (e)=>{
-    console.log('clicked', e);
-})
+nextBtn.addEventListener('click', ()=>{
+    // console.log(billAmount.value);
+    billAmountVal = billAmount.value;
+
+    // billAmountVal is a string type
+    if(billAmountVal.length != 0){
+        // showing cashContainer
+        cashContainer.style.display = 'block';
+        
+        checkBtn.addEventListener('click',()=>{
+            cashGivenVal = cashGiven.value;
+            
+            // showing result 
+            returnCashContainer.style.display = 'block'
+        });
+    }else{
+        warningMessage.style.display = 'block'
+    }
+});
